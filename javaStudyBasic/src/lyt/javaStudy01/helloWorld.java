f2e299a042cd73524e9511e46f24fbe0c46a38f8
BeiLingfeng <2512492119@qq.com>
2022-02-09 03:10:39 +0800

    第一次提交

---
+++ javaStudyBasic/src/lyt/javaStudy01/helloWorld.java
@@ -0,0 +1,14 @@
+package lyt.javaStudy01;
+
+/**
+ * @program: javaStu
+ * @ClassName helloWorld
+ * @description:
+ * @author: 罗业腾
+ * @create: 2022-02-09 03:08
+ **/
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
